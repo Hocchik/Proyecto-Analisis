3ee9e87cc4f91bda519f13fda0c76e05bd97e8b2
Hocchi <JoshyCristiano@outlook.es>
2024-11-20 22:58:29 -0500

    Fix

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,123 +1,146 @@
-import User from "../models/user.model.js";
-import bcrypt from "bcryptjs";
+import { getConnection } from "../database/connection.js";
 import {createAccessToken} from "../libs/jwt.js";
-import jwt from "jsonwebtoken";
-import { TOKEN_SECRET } from "../config.js";
+import sql from 'mssql'
 
+export const register = async (req, res) =>{
 
+    const {Nombre, Apellido, NombreUsuario, Contrasena, Telefono, CorreoElectronico} = req.body;
 
-export const register = async (req, res) => {
+    const pool = await getConnection();
+
+    try {
+        const UserFound = await pool.request()
+        .input('NombreUsuario', sql.NVarChar, NombreUsuario)
+        .input('Contrasena', sql.NVarChar, Contrasena)
+        .input('CorreoElectronico', sql.NVarChar,  CorreoElectronico)
+        .query('Select * from Clientes where NombreUsuario = @NombreUsuario and Contrasena = @Contrasena and CorreoElectronico = @CorreoElectronico')
+
+        if(UserFound){
+            return res.status(200).json({message: 'User already exists'})
+        }
+
+        const result = await pool.request()
+        .input('NombreUsuario', sql.NVarChar, NombreUsuario)
+        .input('Contrasena', sql.NVarChar, Contrasena)
+        .input('CorreoElectronico', sql.NVarChar,  CorreoElectronico)
+        .input('Nombre', sql.NVarChar, Nombre)
+        .input('Apellido', sql.NVarChar, Apellido)
+        .input('Telefono', sql.NVarChar, Telefono)
+        .query('INSERT INTO Clientes (NombreUsuario, Contrasena, CorreoElectronico, Nombre, Apellido, Telefono) VALUES (@NombreUsuario, @Contrasena, @CorreoElectronico, @Nombre, @Apellido, @Telefono)')
+
+        console.log(result)
+
+        const token = await createAccessToken({ id: result.ClienteID });
     
-    const{email, password, username} = req.body;
-
-    try{
-        const userFound = await User.findOne({email});
-        if(userFound) 
-            return res.status(400).json(['The email is already in use'])
-
-
-        const passwordHash= await bcrypt.hash(password, 10);
-        //Esto genera un String cualquiera :3
-
-        const newUser = new User({
-            username,
-            email,
-            password : passwordHash,
-        });
-
-
-        console.log(newUser);
-        const userSaved = await newUser.save();
-        
-        /* Problemas Aquí */
-        const token = await createAccessToken({ id: userSaved._id });
-        
         res.cookie("token", token);
-        
+
         res.json({
-            id: userSaved._id,
-            username: userSaved.username,
-            email: userSaved.email,
-            createdAt: userSaved.createdAt,
-            updatedAt: userSaved.updatedAt,
-        });
+            ClienteID: result.recordset[0].ClienteID,
+            Nombre: Nombre,
+            Apellido: Apellido,
+            NombreUsuario: NombreUsuario,
+            Contrasena: Contrasena,
+            CorreoElectronico: CorreoElectronico,
+            Telefono: Telefono
+        })
 
-
-    }catch(error){
+    } catch (error) {
         res.status(500).json({ message: error.message });
     }
 
-};
+}
 
-export const login = async (req, res) => {
+export const login = async (req, res) =>{
+    const { NombreUsuario, Contrasena } = req.body;
+
+    const pool = await getConnection();
+
+    try {
+        const UserFound = await pool.request()
+        .input('NombreUsuario', sql.NVarChar, NombreUsuario)
+        .input('Contrasena', sql.NVarChar, Contrasena)
+        .query('Select * from Clientes where NombreUsuario = @NombreUsuario and Contrasena = @Contrasena')
+
+        if(!UserFound){
+            return res.status(404).json({message: 'User not found'})
+        }
+
+        const token = await createAccessToken({ id: UserFound.ClienteID });
     
-    /* Puedo colocar en lugar de Email, username */
-    const{ email, password } = req.body;
-
-    try{
-
-        const userFound = await User.findOne({email}) /* Aqui tmb se cambiaria a username */
-        if(!userFound) return res.status(400).json({ message: "User not found"});
-
-        const isMatch= await bcrypt.compare(password, userFound.password);
-        if(!isMatch) return res.status(400).json({ message: "Incorrect Password"});
-
-    
-        /* Problemas Aquí */
-        const token = await createAccessToken({ id: userFound._id });
-        
         res.cookie("token", token);
-        
+
         res.json({
-            id: userFound._id,
-            username: userFound.username,
-            email: userFound.email,
-            createdAt: userFound.createdAt,
-            updatedAt: userFound.updatedAt,
-        });
+            ClienteID: UserFound.recordset[0].ClienteID,
+            Nombre: UserFound.recordset[0].Nombre,
+            Apellido: UserFound.recordset[0].Apellido,
+            NombreUsuario: UserFound.recordset[0].NombreUsuario,
+            Contrasena: UserFound.recordset[0].Contrasena,
+            CorreoElectronico: UserFound.recordset[0].CorreoElectronico,
+            Telefono: UserFound.recordset[0].Telefono
+        })
 
-
-    }catch(error){
+    } catch (error) {
         res.status(500).json({ message: error.message });
     }
 
-};
 
-export const logout = async (req, res) => {
+}
+
+export const logout = async (req, res) =>{
     res.cookie('token', "", {
         expires: new Date(0)
     });
     return res.sendStatus(200);
-} 
-
-export const profile = async (req, res) => {
-    const userFound = await User.findById(req.user.id);
-    if(!userFound)  return res.status(400).json({message: "User not found"});
-    
-    return res.json({
-        id: userFound._id,
-        username: userFound.username,
-        email: userFound.email,
-        createdAt: userFound.createdAt,
-        updatedAt: userFound.updatedAt,
-    })
-    res.send('profile');
 }
 
-export const verifyToken = async (req, res) => {
-    const {token}= req.cookies
-        if(!token) return res.status(401).json({message: 'Unauthorized'})
+export const profile = async (req, res) =>{
+    
+    const {ClienteID} = req.body
 
-        jwt.verify(token, TOKEN_SECRET, async (err, user) => {
+    const pool = await getConnection();
+    
+    try {
+        const UserFound = await pool.request()
+        .input('ClienteID', sql.NVarChar, ClienteID)
+        .query('Select * from Clientes where ClienteID = @ClienteID')
+
+        if(!UserFound){
+            return res.status(404).json({message: 'User not found'})
+        }
+
+        return res.json({
+            ClienteID: UserFound.recordset[0].ClienteID,
+            Nombre: UserFound.recordset[0].Nombre,
+            Apellido: UserFound.recordset[0].Apellido,
+            NombreUsuario: UserFound.recordset[0].NombreUsuario,
+            Contrasena: UserFound.recordset[0].Contrasena,
+            CorreoElectronico: UserFound.recordset[0].CorreoElectronico,
+            Telefono: UserFound.recordset[0].Telefono
+        })
+
+        
+    } catch (error) {
+        
+    }
+}
+
+export const verifyToken = async (req, res) =>{
+    const {token} =req.cookies
+    if(!token)  return res.status(401).json({message: 'Unauthorized'})
+
+        const pool = await getConnection();
+
+        jwt.verify(token, TOKEN_SECRET, async (err, ClienteID) => {
             if(err) return res.status(401).json({message: 'Unauthorized'}) 
-                const userFound = await User.findById(user.id) 
+                const AdminFound = await pool.request()
+            .input('ClienteID', sql.NVarChar, ClienteID)
+            .query('Select * from Clientes where ClienteID = @ClienteID')
 
-            if(!userFound) return res.status(401).json({message: 'Unauthorized'}) 
+            if(!AdminFound) return res.status(401).json({message: 'Unauthorized'}) 
 
                 return res.json({
-                    id: userFound._id,
-                    username: userFound.username,
-                    email: userFound.email,
+                    ClienteID: UserFound.recordset[0].ClienteID,
+                    NombreUsuario: UserFound.recordset[0].NombreUsuario,
                 })
 
         })
--- src/controllers/reservas.controller.js
+++ src/controllers/reservas.controller.js
@@ -1,5 +1,5 @@
 import { getConnection } from "../database/connection.js";
-import sql, { MAX } from 'mssql'
+import sql from 'mssql'
 
 export const getReservaciones = async (req, res) =>{
     const pool = await getConnection();
@@ -9,7 +9,18 @@ export const getReservaciones = async (req, res) =>{
     res.json(result)
 }
 
-export const createReservaciones = async (req, res) =>{
+export const getReservacion = async (req, res) =>{
+    const pool = await getConnection();
+    
+    const result = await pool.request()
+    .input('ReservacionID',  sql.Int, req.params.id)
+    .query('Select * from Reservaciones where ReservacionID = @ReservacionID')
+
+    res.json(result);
+    console.log(result)
+} 
+
+export const createReservacion = async (req, res) =>{
     console.log(req.body);
 
     const pool = await getConnection();
@@ -17,9 +28,60 @@ export const createReservaciones = async (req, res) =>{
     const result = await pool.request()
     .input('MesaID', sql.Int, req.body.MesaID)
     .input('FechaReservacion', sql.DateTime, req.body.FechaReservacion)
-    .input('HoraInicio', sql.DateTime, req.body.HoraInicio)
     .input('ClienteID', sql.Int, req.body.ClienteID)
-    .input('Observaciones', sql.NVarChar(MAX), req.body.Observaciones)
-    .query('Insert into Reservaciones (MesaID, FechaReservacion, HoraInicio, ClienteID, Observaciones) values (@MesaID, @FechaReservacion, @HoraInicio, @ClienteID, @Observaciones)')
-    res.json(result)
+    .input('Observaciones', sql.VarChar, req.body.Observaciones)
+    .query('Insert into Reservaciones (MesaID, FechaReservacion, ClienteID, Observaciones) values (@MesaID, @FechaReservacion, @ClienteID, @Observaciones); Select SCOPE_IDENTITY() as ReservacionID')
+    
+    console.log(result)
+
+    res.json({
+        id: result.recordset[0].ReservacionID,
+        MesaID: req.body.MesaID,
+        FechaReservacion: req.body.FechaReservacion,
+        ClienteID: req.body.ClienteID,
+        Observaciones: req.body.Observaciones
+    })
+
+} 
+
+export const putReservacion = async (req, res) =>{
+    console.log(req.body);
+    const pool = await getConnection();
+
+    const result = await pool.request()
+    .input('ReservacionID', sql.Int, req.params.id)
+    .input('MesaID', sql.Int, req.body.MesaID)
+    .input('FechaReservacion', sql.DateTime, req.body.FechaReservacion)
+    .input('ClienteID', sql.Int, req.body.ClienteID)
+    .input('Observaciones', sql.VarChar, req.body.Observaciones)
+    .query('Update Reservaciones set MesaID= @MesaID, FechaReservacion = @FechaReservacion, ClienteID= @ClienteID, Observaciones= @Observaciones where ReservacionID = @ReservacionID')
+    
+    if(result.rowsAffected[0] === 0){
+        return res.status(404).json({message:'Reservacion no encontrada'})
+    }
+
+    res.json({
+        id: req.params.id,
+        MesaID: req.body.MesaID,
+        FechaReservacion: req.body.FechaReservacion,
+        ClienteID: req.body.ClienteID,
+        Observaciones: req.body.Observaciones
+    })
+}
+
+export const deleteReservacion = async (req, res) =>{
+    const pool = await getConnection();
+    
+    const result = await pool.request()
+    .input('ReservacionID',  sql.Int, req.params.id)
+    .query('Delete from Reservaciones where ReservacionID = @ReservacionID')
+
+    console.log(result)
+
+    if(result.rowsAffected[0] === 0){
+        return res.status(404).json({message:'Reservacion no encontrada'})
+    }
+
+    return res.json({message:'Reservacion eliminada con exito'})
+
 }
